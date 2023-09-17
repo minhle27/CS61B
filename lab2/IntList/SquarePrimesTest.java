fbf33a245eab4436d070f2d19150aeb7f7af7925
Minh Le <qminh279@gmail.com>
2023-09-17 16:29:00 -0400

    submit lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesNotSimple() {
+        IntList lst = IntList.of(2, 1, 7, 17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 1 -> 49 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
 }
