f3205bd2268ce9d9ecbc5b81a7a7503b1e50e620
Minh Le <qminh279@gmail.com>
2023-11-08 13:35:07 -0500

    finish lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,17 +1,12 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Minh Le
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** modified version of nextNumber */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        if (n % 2 == 0) return n / 2;
+        return n * 3 + 1;
     }
 
     public static void main(String[] args) {
