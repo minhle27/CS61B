453cc316e0747b4fcc0fb0ce090c404a35656c1f
Minh Le <qminh279@gmail.com>
2023-12-31 01:54:34 -0500

    implemented helper methods for interacting with file system

--- proj2/gitlet/Helpers.java
+++ proj2/gitlet/Helpers.java
@@ -1,2 +1,21 @@
-package gitlet;public class Helpers {
+package gitlet;
+
+import java.io.File;
+import java.io.IOException;
+
+import static gitlet.Repository.*;
+import static gitlet.Utils.*;
+
+/** This class contains useful helper methods
+ *
+ *  @author Minh Le
+ */
+
+public class Helpers {
+    public static void saveCommit(Commit obj) throws IOException {
+        // TODO (hint: don't forget dog names are unique)
+        File saveFile = join(OBJECTS_DIR, sha1(obj.toString()));
+        saveFile.createNewFile();
+        writeObject(saveFile, obj);
+    }
 }
