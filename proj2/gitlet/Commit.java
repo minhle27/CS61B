470cc76cbf86bb89d4a185275ff8b9514336ce09
Minh Le <qminh279@gmail.com>
2024-01-14 15:47:08 -0500

    add 2nd par ref to Commit Class

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -29,22 +29,26 @@ public class Commit implements Serializable, Dumpable {
     private Date timestamp;
 
     /** The parent reference of this Commit. */
-    private String par;
+    private String par1;
+
+    private String par2; // for merge commit
 
     /** The reference of mapping data structure. */
     private String mapping;
 
-    public Commit(String message, String par, String mapping) {
+    public Commit(String message, String par1, String mapping) {
         this.message = message;
         this.timestamp = new Date();
-        this.par = par;
+        this.par1 = par1;
         this.mapping = mapping;
+        this.par2 = "";
     }
 
     public Commit() {
         this.message = "initial commit";
         this.timestamp = new Date(0);
-        this.par = "";
+        this.par1 = "";
+        this.par2 = "";
         this.mapping = "";
     }
 
@@ -56,8 +60,12 @@ public class Commit implements Serializable, Dumpable {
         return timestamp;
     }
 
-    public String getPar() {
-        return par;
+    public String getPar1() {
+        return par1;
+    }
+
+    public String getPar2() {
+        return par2;
     }
 
     public String getMappingTree() {
@@ -69,8 +77,9 @@ public class Commit implements Serializable, Dumpable {
         StringBuilder sb = new StringBuilder();
         sb.append("Commit Message: ").append(message).append("\n");
         sb.append("Timestamp: ").append(timestamp).append("\n");
-        sb.append("Parent Commit: ").append(par).append("\n");
+        sb.append("Parent Commit: ").append(par1).append("\n");
         sb.append("Commit Mapping: ").append(mapping).append("\n");
+        sb.append("2nd Parent Commit: ").append(par2).append("\n");
         return sb.toString();
     }
 
